447578a938df595422903640446a72725ef3fb66
Victor Daniel Peraza Bolaños <00143320@uca.edu.sv>
2021-11-13 13:31:37 -0600

    Components created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+
+import Pokedex from './components/pokedex/pokedex';
+
 function App() {
-    return (
-    <div>   
+  return (
+    <div className="w-full min-h-fullscreen flex flex-col bg-gray-200">
+      <header className="w-full h-16 bg-gray-300 sticky top-0 left-0 flex justify-center items-center text-black font-oswald z-10"> National Pokedex </header>
+
+      <main className="p-8 flex flex-col justify-center gap-8">
+        <Pokedex pokemons={pokemons} />
+      </main>
     </div>
   );
 }
